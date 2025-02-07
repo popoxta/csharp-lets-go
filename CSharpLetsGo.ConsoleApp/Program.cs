8f54524c663b19effb20a9df1eceb74d021eb6c7
lari <popoxta@gmail.com>
2025-02-07 14:11:34 +1300

    Add logic to handle double and triple rolls

--- CSharpLetsGo.ConsoleApp/Program.cs
+++ CSharpLetsGo.ConsoleApp/Program.cs
@@ -14,10 +14,20 @@ SeparateLines();
 // 0.2 Random Dice Roll Logic
 
 var diceRolls = new int[3].Select(_ => GetRandomDiceRoll()).ToArray();
-var total = diceRolls.Sum();
 
-foreach (var roll in diceRolls.Select((value, i) => new { value, i })) 
+var isDouble = diceRolls.Any(roll => diceRolls.Count(el => el == roll) >= 2);
+var isTriple = diceRolls.All(roll => roll == diceRolls[0]);
+
+var rollSum = diceRolls.Sum();
+var total = isTriple ? rollSum + 3 : isDouble ? rollSum + 2 : rollSum;
+
+foreach (var roll in diceRolls.Select((value, i) => new { value, i }))
     Console.WriteLine($"Dice roll {roll.i}: {roll.value}");
+
+Console.WriteLine();
+
+if (isTriple) Console.WriteLine("You got a triple! +3 score to total!");
+else if (isDouble) Console.WriteLine("You got a double! +2 score to total!");
 
 Console.WriteLine($"Total Score of the rolls is {total}!!!");
 Console.WriteLine(total > 14 ? "You won the dice roll!" : "You lost the dice roll!");
