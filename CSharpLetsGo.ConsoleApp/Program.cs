35bf7bd45da6dbb8ae23ee398449554fef411254
lari <popoxta@gmail.com>
2025-02-07 13:01:13 +1300

    Create a new Random instance using target typed constructor invocation

--- CSharpLetsGo.ConsoleApp/Program.cs
+++ CSharpLetsGo.ConsoleApp/Program.cs
@@ -1,4 +1,8 @@
 ﻿var dice = new Random();
 var roll = dice.Next(1, 7);
 
+Random secondDice = new();
+
 Console.WriteLine($"The dice roll is: {roll}!!!");
+
+Console.WriteLine($"The dice roll is: {secondDice.Next(1, 7)}!!!");
