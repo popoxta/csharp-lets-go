113ea2f862493d74b71048017a625a68615204f5
lari <popoxta@gmail.com>
2025-02-07 14:04:01 +1300

    Add 0.2 module dice roll with ternary, foreach

--- CSharpLetsGo.ConsoleApp/Program.cs
+++ CSharpLetsGo.ConsoleApp/Program.cs
@@ -11,6 +11,19 @@ Console.WriteLine($"The larger value is {largerValue}!!!\nAin't that obvious!");
 
 SeparateLines();
 
+// 0.2 Random Dice Roll Logic
+
+var diceRolls = new int[3].Select(_ => GetRandomDiceRoll()).ToArray();
+var total = diceRolls.Sum();
+
+foreach (var roll in diceRolls.Select((value, i) => new { value, i })) 
+    Console.WriteLine($"Dice roll {roll.i}: {roll.value}");
+
+Console.WriteLine($"Total Score of the rolls is {total}!!!");
+Console.WriteLine(total > 14 ? "You won the dice roll!" : "You lost the dice roll!");
+
 return;
+
+static int GetRandomDiceRoll() => new Random().Next(1, 7);
 
 static void SeparateLines() => Console.WriteLine("\n----\n");
