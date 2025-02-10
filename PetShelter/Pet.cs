4b779954a9fac1c3a8cdaeaf782cf4e639b9928d
lari <popoxta@gmail.com>
2025-02-11 08:56:43 +1300

    Set up Pet class

---
+++ PetShelter/Pet.cs
@@ -0,0 +1,17 @@
+﻿namespace PetShelter;
+
+public enum Species
+{
+    Cat,
+    Dog
+}
+
+public class Pet
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public required Species Species { get; init; }
+    public required int Age { get; init; }
+    public required string PhysicalCondition { get; init; }
+    public string? Personality { get; init; }
+    public string? Name { get; init; }
+}
--- PetShelter/Program.cs
+++ PetShelter/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using PetShelter;
+
 Console.WriteLine("Hello, World!");
+
+var pet = new Pet
+{
+    Age = 12,
+    PhysicalCondition = "Perfect!",
+    Species = Species.Cat
+};
+
+Console.WriteLine(pet.Id);
