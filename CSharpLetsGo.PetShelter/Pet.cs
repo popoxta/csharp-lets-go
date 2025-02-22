79b5b824b14361c14d115017eb7052862fba5088
lari <popoxta@gmail.com>
2025-02-23 11:04:05 +1300

    Begin listing out options for pet shelter program

--- CSharpLetsGo.PetShelter/Pet.cs
+++ CSharpLetsGo.PetShelter/Pet.cs
@@ -11,7 +11,7 @@ public class Pet
     public Guid Id { get; } = Guid.NewGuid();
     public required Species Species { get; init; }
     public required int Age { get; init; }
+    public required string Name { get; init; }
     public required string PhysicalCondition { get; init; }
     public string? Personality { get; init; }
-    public string? Name { get; init; }
 }
--- CSharpLetsGo.PetShelter/Program.cs
+++ CSharpLetsGo.PetShelter/Program.cs
@@ -1,44 +1,70 @@
-﻿using PetShelter;
+﻿using CSharpLetsGo.Shared;
+using PetShelter;
 
 const int maxPets = 8;
 
+const string optionsPrompt =
+    """
+    Welcome to PetFriends!
+    Your menu options are:
+    1. Exit
+    2. List all pets
+    3. Add pet
+    4. Edit pet age
+    5. Edit pet personality
+    6. Display cats with specific characteristics
+    7. Display dogs with specific characteristics
+    
+    Please select an option.
+    """;
 
-Pet[] cats = [new()
+
+Pet[] cats =
+[
+    new()
     {
         Species = Species.Cat,
+        Name = "Josuke",
         Age = 5,
         PhysicalCondition = "Good"
     },
     new()
     {
         Species = Species.Cat,
+        Name = "Johnny",
         Age = 8,
         PhysicalCondition = "Could be better"
     },
     new()
     {
         Species = Species.Cat,
+        Name = "Joseph",
         Age = 2,
         PhysicalCondition = "Great"
     },
 ];
 
 
-Pet[] dogs = [new()
+Pet[] dogs =
+[
+    new()
     {
         Species = Species.Dog,
+        Name = "Lucy",
         Age = 3,
         PhysicalCondition = "Very good"
     },
     new()
     {
         Species = Species.Dog,
+        Name = "Doppio",
         Age = 7,
         PhysicalCondition = "Small"
     },
     new()
     {
         Species = Species.Dog,
+        Name = "Emporio",
         Age = 10,
         PhysicalCondition = "Wrinkly"
     },
@@ -48,14 +74,36 @@ var menuSelection = MenuOptions.Default;
 
 do
 {
-    Console.WriteLine(
-        """"
-        
-        """"
-        );
-    Console.ReadLine();
+    menuSelection = (MenuOptions)Prompt.GetInt(optionsPrompt, min: 1, max: Enum.GetValues(typeof(MenuOptions)).Length);
 
-} while(false);
+    switch (menuSelection)
+    {
+        case MenuOptions.Exit:
+            Console.WriteLine("Goodbye!");
+            return;
+        case MenuOptions.List:
+            Console.WriteLine("All pets:");
+            Console.WriteLine("Species\t\tName\t\tAge");
+            foreach (var pet in cats.Concat(dogs).ToArray())
+                Console.WriteLine($"{pet.Species}\t\t{pet.Name}\t\t{pet.Age}");
+            break;
+        case MenuOptions.Add:
+            break;
+        case MenuOptions.EditAge:
+            break;
+        case MenuOptions.EditPersonality:
+            break;
+        case MenuOptions.DisplayCatsWithCharacteristic:
+            break;
+        case MenuOptions.DisplayDogsWithCharacteristic:
+            break;
+        case MenuOptions.Default:
+            break;
+    }
+
+    ;
+    Prompt.PressEnterToContinue();
+} while (menuSelection != MenuOptions.Exit);
 
 internal enum MenuOptions
 {
