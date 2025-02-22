2fcc4110074b266cac0d35a6163c7ea28d40ca17
lari <popoxta@gmail.com>
2025-02-23 11:45:00 +1300

    Add yes/no helper and private method to write prompt if not null

--- CSharpLetsGo.Shared/Prompt.cs
+++ CSharpLetsGo.Shared/Prompt.cs
@@ -2,6 +2,11 @@
 
 public static class Prompt
 {
+    private static void WritePromptIfNotNull(string? prompt)
+    {
+        if (prompt is not null) Console.WriteLine(prompt);
+    }
+
     /// <summary>
     /// Prompts the user for an integer input until a valid integer input is given.
     /// </summary>
@@ -12,7 +17,7 @@ public static class Prompt
     public static int GetInt(string? prompt, int max = int.MaxValue, int min = int.MinValue)
     {
         int result;
-        if (prompt is not null) Console.WriteLine(prompt);
+        WritePromptIfNotNull(prompt);
         while (!int.TryParse(Console.ReadLine(), out result) && result >= min && result <= max) ;
         return result;
     }
@@ -20,14 +25,28 @@ public static class Prompt
     /// <summary>
     /// Prompts the user for a string input until a valid input is given.
     /// </summary>
-    /// <param name="prompt"></param>
+    /// <param name="prompt">Optional prompt that is displayed to the user.</param>
     /// <returns></returns>
     public static string GetString(string? prompt)
     {
         var input = "";
-        if (prompt is not null) Console.WriteLine(prompt);
+        WritePromptIfNotNull(prompt);
         while (string.IsNullOrEmpty(input)) input = Console.ReadLine()?.Trim() ?? "";
         return input;
+    }
+
+    /// <summary>
+    /// Returns a bool indicating if the user entered Yes (y) or No (n)
+    /// </summary>
+    /// <param name="prompt">Optional prompt that is displayed to the user.</param>
+    /// <returns></returns>
+    public static bool YesOrNo(string? prompt)
+    {
+        WritePromptIfNotNull(prompt);
+        Console.WriteLine("y: Yes\nn: No");
+        var input = "";
+        while (input is not ("y" or "n")) input = Console.ReadLine()?.Trim() ?? "";
+        return input == "y";
     }
 
     /// <summary>
