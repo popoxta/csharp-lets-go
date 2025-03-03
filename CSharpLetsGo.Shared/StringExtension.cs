77816009b22dfc815b3f230c8eeb36ecc0fdb001
lari <popoxta@gmail.com>
2025-03-03 16:49:20 +1300

    Add doc for return type on Truncate

--- CSharpLetsGo.Shared/StringExtension.cs
+++ CSharpLetsGo.Shared/StringExtension.cs
@@ -7,7 +7,7 @@ public static class StringExtension
     /// </summary>
     /// <param name="str">String to truncate</param>
     /// <param name="maxLength">Maximum length of string</param>
-    /// <returns></returns>
+    /// <returns>Truncated string</returns>
     public static string Truncate(this string str, int maxLength = 10)
     {
         return str.Length <= maxLength ? str : str[..maxLength] + "...";
