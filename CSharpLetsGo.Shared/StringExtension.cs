5b81f85bea2c204d9ac71eb7d78bc0deef731c58
lari <popoxta@gmail.com>
2025-03-03 16:48:14 +1300

    Create truncate method

---
+++ CSharpLetsGo.Shared/StringExtension.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharpLetsGo.Shared;
+
+public static class StringExtension
+{
+    /// <summary>
+    /// Truncates a string to x given characters, adding a trailing "..."
+    /// </summary>
+    /// <param name="str">String to truncate</param>
+    /// <param name="maxLength">Maximum length of string</param>
+    /// <returns></returns>
+    public static string Truncate(this string str, int maxLength = 10)
+    {
+        return str.Length <= maxLength ? str : str[..maxLength] + "...";
+    }
+}
