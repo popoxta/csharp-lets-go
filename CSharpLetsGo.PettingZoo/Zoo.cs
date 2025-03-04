f95a26e2523803985cb8b68c5559da84499d7b5a
lari <popoxta@gmail.com>
2025-03-05 06:52:26 +1300

    Fix indexing

--- CSharpLetsGo.PettingZoo/Zoo.cs
+++ CSharpLetsGo.PettingZoo/Zoo.cs
@@ -16,13 +16,15 @@ public class Zoo
     {
         if (VisitingGroups == null) throw new InvalidOperationException("VisitingGroups is null");
         var animals = GetShuffledAnimals();
-        var animalsPerGroup = Animals.Length / VisitingGroups.Length;
+        var animalsPerGroup = animals.Length / VisitingGroups.Length;
 
         foreach (var (group, i) in VisitingGroups.Select((group, i) => (group, i)))
         {
-            var animalStartIndex = i * 3;
-            var animalsForGroup = animals[animalStartIndex..animalsPerGroup].ToArray();
-            group.AssignedAnimals = animalsForGroup;
+            var animalStartIndex = i * animalsPerGroup;
+            var animalsAssignedToGroup =
+                animals[animalStartIndex..(animalsPerGroup + animalStartIndex)]
+                    .ToArray();
+            group.AssignedAnimals = animalsAssignedToGroup;
         }
     }
 }
