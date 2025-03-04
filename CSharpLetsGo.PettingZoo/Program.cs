dcdc5499d16d981f2cf2afe4081a1388600e2c48
lari <popoxta@gmail.com>
2025-03-04 16:48:01 +1300

    Create array of animals

--- CSharpLetsGo.PettingZoo/Program.cs
+++ CSharpLetsGo.PettingZoo/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+using CSharpLetsGo.PettingZoo;
 
-Console.WriteLine("Hello, World!");
+string[] animalTypes =
+[
+    "alpacas", "capybaras", "chickens", "ducks", "emus", "geese",
+    "goats", "iguanas", "kangaroos", "lemurs", "llamas", "macaws",
+    "ostriches", "pigs", "ponies", "rabbits", "sheep", "tortoises",
+];
+
+var animals = animalTypes.Select(animal => new Animal
+{
+    AnimalType = animal,
+}).ToArray();
+
